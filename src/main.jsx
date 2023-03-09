e76fe19b69b46fa07cde694babcdc192d3364428
Don Koscheka <donkos@live.com>
2023-03-09 17:03:49 -0500

    First app with react router

---
+++ src/main.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter,RouterProvider,} from 'react-router-dom';
+import ErrorPage from "./error-page";
+import Contact, { loader as contactLoader,} from './routes/contact';
+import EditContact, {action as editAction } from './routes/edit';
+import Root, { loader as rootLoader, action as rootAction } from './routes/root';
+import {action as deleteAction} from "./routes/delete"
+import './index.css';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    action: rootAction,
+    children:[
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+        loader : contactLoader,
+      },
+      {
+        path:"contacts/:contactId/edit",
+        element: <EditContact />,
+        loader: contactLoader,
+        action: editAction,
+      },
+      {
+        path:"contacts/:contactId/delete",
+        action: deleteAction,
+      }
+    ]
+  },
+
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
