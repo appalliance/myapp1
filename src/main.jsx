d7d6c2a7f43045cb90a166d5e31c488844bb48c1
Don Koscheka <donkos@live.com>
2023-03-10 10:42:35 -0500

    Completed all tutorial steps

--- src/main.jsx
+++ src/main.jsx
@@ -2,38 +2,47 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter,RouterProvider,} from 'react-router-dom';
 import ErrorPage from "./error-page";
-import Contact, { loader as contactLoader,} from './routes/contact';
+import Contact, { loader as contactLoader, action as contactAction } from './routes/contact';
 import EditContact, {action as editAction } from './routes/edit';
 import Root, { loader as rootLoader, action as rootAction } from './routes/root';
-import {action as deleteAction} from "./routes/delete"
+import {action as deleteAction} from "./routes/delete";
+import Index from "./routes/Index";
 import './index.css';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
-    errorElement: <ErrorPage />,
     loader: rootLoader,
     action: rootAction,
+    errorElement: <ErrorPage />,
     children:[
       {
-        path: "contacts/:contactId",
-        element: <Contact />,
-        loader : contactLoader,
+        errorElement:<ErrorPage />,
+        children:[
+          { index: true, element: <Index /> },
+          {
+            path: "contacts/:contactId",
+            element: <Contact />,
+            loader : contactLoader,
+            action : contactAction,
+          },
+          {
+            path:"contacts/:contactId/edit",
+            element: <EditContact />,
+            loader: contactLoader,
+            action: editAction,
+          },
+          {
+            path:"contacts/:contactId/delete",
+            action: deleteAction,
+            errorElement: <div>Oops! There was an error.</div>,
+          }
+        ],
       },
-      {
-        path:"contacts/:contactId/edit",
-        element: <EditContact />,
-        loader: contactLoader,
-        action: editAction,
-      },
-      {
-        path:"contacts/:contactId/delete",
-        action: deleteAction,
-      }
-    ]
-  },
+    ],
 
+  },
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
